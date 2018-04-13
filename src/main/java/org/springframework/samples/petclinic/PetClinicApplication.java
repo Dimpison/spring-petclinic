355a30df9b3e29d3b5e04b9ae3011d0390849ce1
Dimpison <dimakaba4enko@gmail.com>
2018-04-13 16:50:48 +0300

    Update PetClinicApplication.java
    Fix issue

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -26,7 +26,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-pupublic class PetClinicApplication {
+public class PetClinicApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PetClinicApplication.class, args);
